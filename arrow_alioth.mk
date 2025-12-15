bb30804fb79f2cd2cad0a13ee926f128f57779bb
zenzer0s <praveenzalaki200@gmail.com>
2025-12-15 19:21:11 +0530

    .

--- arrow_alioth.mk
+++ arrow_alioth.mk
@@ -18,6 +18,8 @@ TARGET_BOOT_ANIMATION_RES := 1080
 IS_PHONE := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
+ARROW_GAPPS := true
+BYPASS_CHARGE_SUPPORTED := true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
